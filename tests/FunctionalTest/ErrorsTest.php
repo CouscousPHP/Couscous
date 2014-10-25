1b8c29b4a0bfc23ca86961c1dbec1854e2a9c11b
Matthieu Napoli <matthieu@mnapoli.fr>
2014-10-25 20:03:57 +1300

    Fixed tests

--- tests/FunctionalTest/ErrorsTest.php
+++ tests/FunctionalTest/ErrorsTest.php
@@ -6,9 +6,6 @@ class ErrorsTest extends BaseFunctionalTest
 {
     public function testTemplateNeeded()
     {
-        $dir = __DIR__ . '/fixtures';
-        $message = "The template directory doesn't exist: $dir/no-template/source/website";
-
-        $this->assertGenerationError('no-template', $message);
+        $this->assertGenerationError('no-template', "The template directory doesn't exist:");
     }
 }
