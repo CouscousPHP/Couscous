ac06ff9b0c9e90d8350f7931ff93d5dffa057478
Matthieu Napoli <matthieu@mnapoli.fr>
2014-10-27 11:42:07 +1300

    Improved tests for ProcessMarkdownFileName

--- tests/UnitTest/Step/Markdown/ProcessMarkdownFileNameTest.php
+++ tests/UnitTest/Step/Markdown/ProcessMarkdownFileNameTest.php
@@ -2,6 +2,7 @@
 
 namespace Piwik\Tests\UnitTest\Step\Markdown;
 
+use Couscous\Model\LazyFile;
 use Couscous\Model\MarkdownFile;
 use Couscous\Model\Repository;
 use Couscous\Step\Markdown\ProcessMarkdownFileName;
@@ -12,27 +13,24 @@ use Symfony\Component\Console\Output\NullOutput;
  */
 class ProcessMarkdownFileNameTest extends \PHPUnit_Framework_TestCase
 {
+    public function testRenameExtension()
+    {
+        $this->assertFileRenamed('test.html', 'test.md');
+    }
+
     public function testRenameReadme()
     {
-        $file = new MarkdownFile('README.md', '');
-        $repository = new Repository('foo', 'bar');
-        $repository->addFile($file);
-
-        $step = new ProcessMarkdownFileName();
-        $step->__invoke($repository, new NullOutput());
-
-        $files = $repository->getFiles();
-
-        $this->assertCount(1, $files);
-        /** @var MarkdownFile $newFile */
-        $newFile = reset($files);
-
-        $this->assertEquals('index.html', $newFile->relativeFilename);
+        $this->assertFileRenamed('index.html', 'README.md');
     }
 
     public function testRenameReadmeInSubDirectory()
     {
-        $file = new MarkdownFile('foo/README.md', '');
+        $this->assertFileRenamed('foo/index.html', 'foo/README.md');
+    }
+
+    public function testNonMarkdownFileNotRenamed()
+    {
+        $file = new LazyFile('foo.txt', 'foo.txt');
         $repository = new Repository('foo', 'bar');
         $repository->addFile($file);
 
@@ -45,6 +43,25 @@ class ProcessMarkdownFileNameTest extends \PHPUnit_Framework_TestCase
         /** @var MarkdownFile $newFile */
         $newFile = reset($files);
 
-        $this->assertEquals('foo/index.html', $newFile->relativeFilename);
+        $this->assertEquals('foo.txt', $newFile->relativeFilename);
+        $this->assertSame($newFile, $file);
+    }
+
+    private function assertFileRenamed($expected, $filename)
+    {
+        $file = new MarkdownFile($filename, '');
+        $repository = new Repository('foo', 'bar');
+        $repository->addFile($file);
+
+        $step = new ProcessMarkdownFileName();
+        $step->__invoke($repository, new NullOutput());
+
+        $files = $repository->getFiles();
+
+        $this->assertCount(1, $files);
+        /** @var MarkdownFile $newFile */
+        $newFile = reset($files);
+
+        $this->assertEquals($expected, $newFile->relativeFilename);
     }
 }
