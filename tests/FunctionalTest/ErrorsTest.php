f5c9ed21fb4916d0a82e4bcf28342b5417227814
Matthieu Napoli <matthieu@mnapoli.fr>
2014-11-01 18:26:05 +1300

    Fixing tests on Travis

--- tests/FunctionalTest/ErrorsTest.php
+++ tests/FunctionalTest/ErrorsTest.php
@@ -6,6 +6,6 @@ class ErrorsTest extends BaseFunctionalTest
 {
     public function testMissingTemplate()
     {
-        $this->assertGenerationError('missing-template-file', 'There was an error while rendering the file "index.html" with the template "home.twig": Template "home.twig" is not defined.');
+        $this->assertGenerationError('missing-template-file', 'Template "home.twig" is not defined.');
     }
 }
