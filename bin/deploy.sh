1851d046311e934961ec0179c72096f8f192dc9c
Matthieu Napoli <matthieu@mnapoli.fr>
2014-11-30 18:29:00 +1300

    Updated the deploy script to use `couscous deploy`

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 if [ ! "$TRAVIS_BRANCH" = "master" ]
 then
-    echo "[NOT DEPLOYED] Current branch is not master"
+    echo "[NOT DEPLOYED] Deploying Couscous only for master branch"
     exit 1
 fi
 
 if [ ! "$TRAVIS_PULL_REQUEST" = false ]
 then
-    echo "[NOT DEPLOYED] Not pushing documentation for pull requests"
+    echo "[NOT DEPLOYED] Not deploying Couscous for pull requests"
     exit 1
 fi
 
 mkdir out
-echo "Starting deploy..."
+echo "Deploying Couscous..."
 (
-cd out
-git init
 git config user.name ${GIT_NAME}
 git config user.email ${GIT_EMAIL}
-cp ../website/* ./
-git add .
-git commit -m "Deployed to Github Pages"
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+bin/couscous deploy --repository="https://${GH_TOKEN}@${GH_REF}"
 )
