07d4de368029e973ab64501cd31e6c995fb48cb3
Henrique Moody <henriquemoody@gmail.com>
2015-02-08 13:30:41 -0200

    Improve rewrite of markdown links
    - Replace "README.md" files to "index.html"
    - Replace uppercase links to lower case

--- src/Module/Markdown/Step/RewriteMarkdownLinks.php
+++ src/Module/Markdown/Step/RewriteMarkdownLinks.php
@@ -14,8 +14,9 @@ use Couscous\Step;
 class RewriteMarkdownLinks implements Step
 {
     // OMG regexes...
-    const MARKDOWN_LINK_REGEX = '#\[([^\]]+)\]\(([^\)]+)\.md\)#';
-    const REGEX_REPLACEMENT   = '[$1]($2.html)';
+    const MARKDOWN_LINK_REGEX  = '#\[([^\]]+)\]\(([^\)]+)\.md\)#';
+    const REGEX_REPLACEMENT    = '[$1]($2.html)';
+    const UPPERCASE_LINK_REGEX = '#\[(?:[^\]]+)\]\((.+[\/])?([A-Z0-9_-]+\.md)\)#';
 
     public function __invoke(Project $project)
     {
@@ -23,11 +24,23 @@ class RewriteMarkdownLinks implements Step
         $markdownFiles = $project->findFilesByType('Couscous\Module\Markdown\Model\MarkdownFile');
 
         foreach ($markdownFiles as $file) {
-            $file->content = preg_replace(
-                self::MARKDOWN_LINK_REGEX,
-                self::REGEX_REPLACEMENT,
+            $content = preg_replace_callback(
+                self::UPPERCASE_LINK_REGEX,
+                array($this, 'replaceUppercase'),
                 $file->content
             );
+            $file->content = preg_replace(self::MARKDOWN_LINK_REGEX, self::REGEX_REPLACEMENT, $content);
         }
+    }
+
+    private function replaceUppercase(array $matches)
+    {
+        $filename = strtolower($matches[2]);
+        $filename = str_replace('.md', '.html', $filename);
+        if ($filename == 'readme.html') {
+            $filename = 'index.html';
+        }
+
+        return str_replace($matches[2].')', $filename.')', $matches[0]);
     }
 }
