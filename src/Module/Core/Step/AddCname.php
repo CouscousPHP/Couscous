1bdd5283ff75232ddea65cc9daab07357a577b53
Leonardo Ruhland <leoruhland@gmail.com>
2015-12-18 13:44:25 -0200

    Create AddCname.php

---
+++ src/Module/Core/Step/AddCname.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Couscous\Module\Core\Step;
+
+use Couscous\Model\Project;
+use Couscous\Step;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Filesystem\Filesystem;
+
+/**
+ * Writes the generated files to disk.
+ *
+ * @author Matthieu Napoli <matthieu@mnapoli.fr>
+ */
+class AddCname implements Step
+{
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    public function __construct(Filesystem $filesystem, LoggerInterface $logger)
+    {
+        $this->filesystem = $filesystem;
+        $this->logger = $logger;
+    }
+
+    public function __invoke(Project $project)
+    {
+        if( isset($project->metadata['cname']) ){
+            $this->filesystem->dumpFile( $project->targetDirectory.'/'.'CNAME', $project->metadata['cname'] );
+            $this->logger->notice('Writing metadata CNAME');
+        }
+    }
+}
