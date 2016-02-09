8bacf62b3e3be0da4f2035f6bdd0806b0e9d272c
Gaultier Boniface <gboniface@wysow.fr>
2016-02-09 19:16:01 +0100

    Add a self update command to update the phar file.

---
+++ src/Application/Cli/SelfUpdateCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Couscous\Application\Cli;
+
+use Couscous\CommandRunner\CommandRunner;
+use Humbug\SelfUpdate\Updater;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Phar file self update.
+ *
+ * @author Gaultier Boniface <gboniface@wysow.fr>
+ */
+class SelfUpdateCommand extends Command
+{
+
+    /**
+     * @var CommandRunner
+     */
+    private $commandRunner;
+
+    public function __construct(CommandRunner $commandRunner)
+    {
+        $this->commandRunner = $commandRunner;
+
+        parent::__construct();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('self-update')
+            ->setAliases(['selfupdate'])
+            ->setDescription('Automatically update the phar file if needed.')
+        ;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        try {
+            $updater = new Updater(null, false);
+            $updater->getStrategy()->setPharUrl('http://couscous.io/couscous.phar');
+            $updater->getStrategy()->setVersionUrl('http://couscous.io/couscous.version');
+
+            $result = $updater->update();
+            $result ? $output->writeln('Phar file updated successfully!') : $output->writeln('No need to update.');
+        } catch (\Exception $e) {
+            die(var_dump($e->getMessage()));
+            $output->writeln('Something wrong happened, please try again later.');
+        }
+    }
+}
