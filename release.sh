d5548ef092e4b6ed893e6081cf65fc768f0c4bc0
Matthieu Napoli <matthieu@mnapoli.fr>
2016-06-02 19:49:56 +0200

    Do not package dev dependencies in the phar
    Duh!

--- release.sh
+++ release.sh
@@ -2,10 +2,23 @@
 
 set -e
 
+# Update
 git pull
-composer update
+
+# Run tests
+composer update -o
 vendor/bin/phpunit
+
+# Clear dev dependencies for a lighter phar
+composer install -o --no-dev
+
+# Generate the phar
 bin/compile
+
+# Generate the website
 mv bin/couscous.phar website/
 cp bin/couscous.version website/
 couscous deploy
+
+# Restore dev dependencies
+composer install
